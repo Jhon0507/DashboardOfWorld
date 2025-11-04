ad8521c5669bcdaebe922f0a775f24086bb2e8fb
jparedessj <paredesugarte527@gmail.com>
2025-11-04 20:08:21 +0100

    modificaciones de la conexion a la BBDD

--- db_utils.py
+++ db_utils.py
@@ -6,11 +6,11 @@ from sqlalchemy.exc import SQLAlchemyError
 
 load_dotenv()
 
-DB_HOST = os.getenv('DB_HOST', '127.0.0.1')
-DB_USER = os.getenv('DB_USER', 'tu_usuario_mariadb')
-DB_PASSWORD = os.getenv('DB_PASSWORD', 'tu_contraseña_mariadb')
-DB_NAME = os.getenv('DB_NAME', 'world')
-DB_PORT = int(os.getenv('DB_PORT', 3307))
+DB_HOST = os.getenv('DB_HOST')
+DB_USER = os.getenv('DB_USER')
+DB_PASSWORD = os.getenv('DB_PASSWORD')
+DB_NAME = os.getenv('DB_NAME')
+DB_PORT = int(os.getenv('DB_PORT'))
 
 MARIADB_URI = f"mariadb+mariadbconnector://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
